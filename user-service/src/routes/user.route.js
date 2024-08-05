98ccec37b95465e5eee771a2721cf7ec1378b0ce
stanlymathai <stanly@math.ai>
2024-08-05 12:47:26 +0530

    chore; add user route.

---
+++ user-service/src/routes/user.route.js
@@ -0,0 +1,22 @@
+const { verifyString } = require('../middlewares/auth.middleware.js');
+const handler = require('../controllers/user.controller.js');
+
+module.exports = (req, res) => {
+    const { method, url } = req;
+
+    return verifyString(req, res, () => {
+        if (url === '/users' && method === 'POST') {
+            return handler.createUser(req, res);
+        }
+
+        const userIdMatch = url.match(/^\/users\/(\w+)$/);
+        if (userIdMatch && method === 'GET') {
+            const userId = userIdMatch[1];
+            return handler.getUserById(req, res, userId);
+        }
+
+        res.writeHead(404, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Not Found' }));
+    });
+};
+
