ebb28b95fadae58ee7a5617e137b5c1c9efea7e1
stanlymathai <stanly@math.ai>
2024-08-06 08:33:59 +0530

    chore; add product model

---
+++ product-service/src/models/product.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('Product', productSchema);
